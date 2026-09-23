6238b16f1272a884f87d26d0b62a336e97594599
pspc18 <pspc18@users.noreply.github.com>
2026-09-23 18:34:17 +0530

    Change login page heading back to Welcome Back

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,7 +18,7 @@
 
             <span class="nv-signin-mark"><x-icon name="sparkles" :size="23" /></span>
 
-            <h1>Hotel Management Software</h1>
+            <h1>Welcome Back</h1>
             <p>Sign in to continue</p>
 
             @if (session('status'))
